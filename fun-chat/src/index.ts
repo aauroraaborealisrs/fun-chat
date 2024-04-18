f403efc33deeeeb54cb6830c9baee67452a9dae3
aauroraaborealisrs <imaginepeach@mail.ru>
2024-04-18 16:51:47 +0300

    feat: add login form validation

--- fun-chat/src/index.ts
+++ fun-chat/src/index.ts
@@ -1,6 +1,4 @@
-              
-              
-              
-              
-              
-console.log("test");
+import LoginForm from "./components/authentication/login";
+
+document.body.innerHTML = '<div id="app"></div>';
+const loginForm = new LoginForm();
